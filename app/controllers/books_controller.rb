0ac605366b8b1dda519c78cc06fb44809fdf3ab6
masaki08 <108650138+masaki08@users.noreply.github.com>
2022-08-04 11:10:21 +0000

    edit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,7 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @new_book = Book.new
   end
 
   def index
@@ -17,27 +18,34 @@ class BooksController < ApplicationController
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
+      @user = current_user
       @books = Book.all
-      render 'index'
+      render :idex
     end
   end
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user == current_user
+      render :edit
+    else
+      redirect_to book_path
+    end
   end
 
   def update
     @book = Book.find(params[:id])
+    @book.user_id = current_user.ids
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
-      render "edit"
+      render :edit
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
