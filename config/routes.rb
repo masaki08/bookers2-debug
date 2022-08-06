d6d9bb4fa08b2a5dfb1ef813a1ea45cd9d246a8e
masaki08 <108650138+masaki08@users.noreply.github.com>
2022-08-06 15:36:09 +0000

    book_comment

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,11 @@ Rails.application.routes.draw do
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
      resource :favorites, only: [:create, :destroy]
+     resource :book_comments, only: [:create, :destroy]
   end
   resources :users, only: [:update, :edit, :index, :show]
-  
-  
+
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  end
- 
