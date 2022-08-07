72e82ba56767e8e302801701df0e92dcb99165cf
masaki08 <108650138+masaki08@users.noreply.github.com>
2022-08-07 15:40:09 +0000

    follwers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'relationships/followings'
+  get 'relationships/followers'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
@@ -11,7 +13,10 @@ Rails.application.routes.draw do
   end
   resources :users, only: [:update, :edit, :index, :show]
 
-
-
+  resouces :users do
+    resource :relationships,only: [:create, :destroy]
+    get 'followings' => 'relationships#follwings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'follwers'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  end
