233f9702c17d9b25e19ff153309a44d7762f829b
masaki08 <108650138+masaki08@users.noreply.github.com>
2022-08-08 12:28:53 +0000

    _info

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -7,7 +7,7 @@ class RelationshipsController < ApplicationController
   # フォロー外すとき
   def destroy
     current_user.unfollow(params[:user_id])
-    redirect_to request.referer  
+    redirect_to request.referer
   end
   # フォロー一覧
   def followings
@@ -15,7 +15,7 @@ class RelationshipsController < ApplicationController
     @users = user.followings
   end
   # フォロワー一覧
-  def followers
+  def follower
     user = User.find(params[:user_id])
     @users = user.followers
   end
--- config/routes.rb
+++ config/routes.rb
@@ -13,9 +13,9 @@ Rails.application.routes.draw do
   end
   resources :users, only: [:update, :edit, :index, :show]
 
-  resouces :users do
+  resources :users do
     resource :relationships,only: [:create, :destroy]
-    get 'followings' => 'relationships#follwings', as: 'followings'
+    get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'follwers'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
